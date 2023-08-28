472f10a4d0f0aeac30dd56812303dd8252ddc6b7
Oh Daehyeon <xzdheogus1@gmail.com>
2023-08-28 19:29:48 +0900

    배포 문제 해결중

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import "./App.scss";
 // Route
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 // components import
 import PortfolioCP  from './components/PortfolioCP';
 
 function App() {
 return(
   <div className="App">
-    <BrowserRouter>
     <Routes>
       <Route path='/' element={<PortfolioCP />} />
       <Route path='*' element={<><h1>404</h1><div>없는페이지에요~</div></>}/>
     </Routes>
-    </BrowserRouter>
   </div>
 );
 };

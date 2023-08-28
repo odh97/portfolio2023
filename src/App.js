e41cab4c5ba62e59f6e10397e1dd03ce01b79dd3
Oh Daehyeon <xzdheogus1@gmail.com>
2023-08-28 18:37:27 +0900

    css 애니메이션 수정

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ import "./App.scss";
 import {Routes, Route, BrowserRouter} from 'react-router-dom';
 // components import
 import PortfolioCP  from './components/PortfolioCP';
-import WaveCP  from './components/WaveCP';
-
-
 
 function App() {
 return(
   <div className="App">
     <BrowserRouter>
     <Routes>
-      <Route path='/index' element={<PortfolioCP />} />
+      <Route path='/' element={<PortfolioCP />} />
       <Route path='*' element={<><h1>404</h1><div>없는페이지에요~</div></>}/>
     </Routes>
     </BrowserRouter>
